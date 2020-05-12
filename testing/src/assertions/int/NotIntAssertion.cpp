865c6386bf5bca5910dfe9c8777c98f1e0364ce8
MarekOchocki <marcopolo97@vp.pl>
2020-05-12 22:22:08 +0200

    fix not int assertion

--- testing/src/assertions/int/NotIntAssertion.cpp
+++ testing/src/assertions/int/NotIntAssertion.cpp
@@ -6,31 +6,31 @@ namespace qu {
 	}
 
 	void NotIntAssertion::toBeEqualTo(int other) const {
-		if (value != other)
+		if (value == other)
 			throw AssertionException("Expected " + std::to_string(value) + " not to be equal to " +
 									 std::to_string(other));
 	}
 
 	void NotIntAssertion::toBeLessThan(int other) const {
-		if (value >= other)
+		if (value < other)
 			throw AssertionException("Expected " + std::to_string(value) + " not to be less than " +
 									 std::to_string(other));
 	}
 
 	void NotIntAssertion::toBeGreaterThan(int other) const {
-		if (value <= other)
+		if (value > other)
 			throw AssertionException("Expected " + std::to_string(value) +
 									 " not to be greater than " + std::to_string(other));
 	}
 
 	void NotIntAssertion::toBeLessOrEqualTo(int other) const {
-		if (value > other)
+		if (value <= other)
 			throw AssertionException("Expected " + std::to_string(value) +
 									 " not to be less or equal to " + std::to_string(other));
 	}
 
 	void NotIntAssertion::toBeGreaterOrEqualTo(int other) const {
-		if (value < other)
+		if (value >= other)
 			throw AssertionException("Expected " + std::to_string(value) +
 									 " not to be greater or equal to " + std::to_string(other));
 	}
